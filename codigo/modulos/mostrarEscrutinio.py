2890694fe6a2c188491885f98db727cd67ec559b
IvanLSanchez <100932334+IvanLSanchez@users.noreply.github.com>
2023-05-29 22:45:40 -0300

    Update mostrarEscrutinio.py

--- codigo/modulos/mostrarEscrutinio.py
+++ codigo/modulos/mostrarEscrutinio.py
@@ -2,34 +2,30 @@
 ICANTIDADCARACTERES = 90
 ICANTIDADCARACTERESENTER = 100
 def mostrarEscrutinio(cantPartidos):
-    sColor1 = "\33[1;37;41m"
-    sColor2 = "\33[1;30;47m"
-    sColor3 = "\33[0;30;47m"
-    sColor4 = "\33[1m"
-    sColor5 = "\33[4m"
-    sColor6 = "\33[0m"
+    sColor1 = "\33[1;37;41m" #Fondo Rojo con letra blanca negrita
+    sColor2 = "\33[1;30;47m" #Fondo Blanco con letra 
+    sColor3 = "\33[0;30;47m" #Fondo Blanco con letra normal
+    sColor4 = "\33[1m" #Negrita
+    sColor5 = "\33[4m" #Subrayado
+    sColor6 = "\33[0;m" #Color Base
 
-    sEnter = sColor2.center(ICANTIDADCARACTERESENTER) + sColor6
-    sSeparador = sColor3 + "_".center(ICANTIDADCARACTERES,"_")
+    ## sEnter = sColor2.center(ICANTIDADCARACTERESENTER) + sColor6
+    sSeparador = sColor3 + "_".center(ICANTIDADCARACTERES,"_") + sColor6
     
     print(sColor1 + "<REGION>".center(ICANTIDADCARACTERES) + sColor6)
 
     print(sColor2 + "ELECCIONES GENERALES 2023".center(ICANTIDADCARACTERES) + sColor6)
-    print(sColor2 + "Categoria: <CARGO>".center(ICANTIDADCARACTERES))
-
-    print(sEnter)
+    print(sColor2 + "Categoria: <CARGO>".center(ICANTIDADCARACTERES) + sColor6)
 
     print(sColor2 + "Electores habilitados: <CANTIDAD_DE_ELECTORES_EN_LA_REGION>".center(ICANTIDADCARACTERES) + sColor6)
     print(sColor2 + "Porcentaje de votantes: <PORCENTAJE>".center(ICANTIDADCARACTERES) + sColor6)
 
     print(sSeparador)
-    print(sEnter)
 
     print(sColor2 + "{:^20} {:^27} {:^20} {:^20}".format("N° LISTA", "PARTIDO POLITICO", "VOTO", "%") + sColor6)
     print(sSeparador)
 
     for i in range (cantPartidos):
-        print(sEnter)
         print(sColor2 + "{:^20} {:^27} {:^20} {:^20}".format(f"<NRO_LISTA_{i+1}>", f"PARTIDO_{i+1}:", "<CANT_VOTOS>", "<PORCENTAJE>") + sColor6)
         print(sColor2 + "{:^20} {:^27} {:^20} {:^20}".format("", "<CANDIDATOS>", "", "") + sColor6)
         print(sSeparador)
