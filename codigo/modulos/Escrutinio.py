748220fe1524a084453f238929f9285559ab9c53
IvanLSanchez <100932334+IvanLSanchez@users.noreply.github.com>
2023-05-30 22:15:47 -0300

    Update Escrutinio.py

--- codigo/modulos/Escrutinio.py
+++ codigo/modulos/Escrutinio.py
@@ -1,56 +1,70 @@
-from modulos import Diccionario
-from modulos import Archivo
+import Diccionario
+import Archivo
 import random
-    
-def asignarVoto(dPadron, dRegiones):
+
+def asignarVoto(dPadron):
+    """Asigna un voto al azar para cada votante"""
     sArchBoletas = "boletas.txt"
     lPartidosMemoria = Archivo.leer(sArchBoletas)
-    lPadron = list(dPadron.keys())
-    lRegiones = list(dRegiones.keys())
-    for iDNI in lPadron:
+    for iDNI in dPadron:
         lVotoDNI = []
-        iVota = random.randint(1, 10)
         iMax = len(lPartidosMemoria)-1
-        sDNI = str(iDNI)
-        sVotoCargo = str(random.randint(1, 4))                
-        if iVota > 1:
-            iPosicion = random.randint(0, iMax)
-            iVotoPartido = lPartidosMemoria[iPosicion][1]
-        else:
-            iVotoPartido = ""
-        lVotoDNI.append(sDNI)
-        for clave in dRegiones:
-            if sDNI in dRegiones[clave]:
-                lVotoDNI.append(clave)
-        lVotoDNI.append(sVotoCargo)
-        lVotoDNI.append(iVotoPartido)
-        #[555, 1, 2, LBD]
-        dPadron[iDNI].append(lVotoDNI)
-        
+        for lDato in dPadron[iDNI]:
+            iVota = random.randint(1, 10)
+            if iVota > 1:
+                iPosicion = random.randint(0, iMax)
+                sVotoPartido = lPartidosMemoria[iPosicion][1]
+            else:
+                sVotoPartido = ""
+            lDato.append(sVotoPartido)
+
+def asignarCargo(dPadron):
+    """Se asigna un voto de cargo público al elector"""
+    iMax = 4
+    dRegiones = {}
+    for iDNI in dPadron:
+        iCargo = random.randint(1, iMax)
+        sCargo = str(iCargo)
+        #Se asigna a la n cantidad de votos del mismo DNI
+        #la misma región 
+        for lDato in dPadron[iDNI]:
+            while sCargo in lDato:
+                iCargo = random.randint(1, iMax)
+                sCargo = str(iCargo)
+            lDato.append(sCargo)
+
 def generarRegion(dPadron, lRegionMemoria):
+    """Genera una región al azar de acuerdo a las obtenidas en lRegionMemoria"""
     iMax = len(lRegionMemoria)-1
     dRegiones = {}
     for iDNI in dPadron:
         iPosicion = random.randint(1, iMax)
-        iVotoRegion = lRegionMemoria[iPosicion][1]
-        
-        #{1:[345, 555], 4:[222]}
+        iRegion = lRegionMemoria[iPosicion][1]
+        sRegion = str(iRegion)
+        #Se asigna a la n cantidad de votos del mismo DNI
+        #la misma región 
+        for lDato in dPadron[iDNI]:
+            lDato.append(sRegion)
+
         #Validación existencia de la región
-        if Diccionario.esClave(dRegiones, iVotoRegion):
-          dRegiones[iVotoRegion].append(str(iDNI))
+        if Diccionario.esClave(dRegiones, iRegion):
+          dRegiones[iRegion].append(str(iDNI))
         else:
-          dRegiones[iVotoRegion] = []
-          dRegiones[iVotoRegion].append(str(iDNI))
+          dRegiones[iRegion] = []
+          dRegiones[iRegion].append(str(iDNI))
     return dRegiones
 
-def generarPadron(iRegistros):
+def generarPadron(iRegistros, lRegionMemoria):
+    """Genera el padrón electoral de acuerdo a la cantidad deseada"""
     dPadron = {}
     for i in range(iRegistros):
-        iVotoDNI = random.randint(1, 99999999)
-        #Validación de la existencia del documento
-        bEsInvalido = Diccionario.esClave(dPadron, iVotoDNI)
-        while bEsInvalido:
-          iVotoDNI = random.randint(1, 99999999)
-          bEsInvalido = Diccionario.esClave(dPadron, iVotoDNI)
-        dPadron[iVotoDNI] = []
+        lVoto = []
+        iDNI = random.randint(1, 99999999)
+        if Diccionario.esClave(dPadron, iDNI):
+            lVoto.append(str(iDNI))
+            dPadron[iDNI].append(lVoto)
+        else:
+            dPadron[iDNI] = []
+            lVoto.append(str(iDNI))
+            dPadron[iDNI].append(lVoto)
     return dPadron
